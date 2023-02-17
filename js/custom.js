2a4b6c7349e251586bc179a29a8be886f5f56d18
Niyas MN <niyas.mn@spericorn.com>
2023-02-17 11:44:07 +0530

    banner updated with assets

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        autoplay:true,
+        prevArrow: '<div class="slick-nav prev-arrow"></div>',
+	    nextArrow: '<div class="slick-nav next-arrow"></div>',
+        dots: true
+});
+  });
