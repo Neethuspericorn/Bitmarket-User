07df7afe40a54f09785f429c95540887d8d4f595
Niyas MN <niyas.mn@spericorn.com>
2023-02-17 12:43:27 +0530

    banner animation stage1

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,31 @@
-$(document).ready(function(){
-    $('.banner-slider').slick({
-        autoplay:true,
-        prevArrow: '<div class="slick-nav prev-arrow"></div>',
-	    nextArrow: '<div class="slick-nav next-arrow"></div>',
-        dots: true
-});
+$(document).ready(function () {
+  $('.banner-slider').slick({
+    autoplay: true,
+    prevArrow: '<div class="slick-nav prev-arrow"></div>',
+    nextArrow: '<div class="slick-nav next-arrow"></div>',
+    dots: true
   });
+  $('.banner-slider').on('init', function(e, slick) {
+    var $firstAnimatingElements = $('div.slick-slide:first-child').find('[data-animation]');
+    doAnimations($firstAnimatingElements);    
+});
+$('.banner-slider').on('beforeChange', function(e, slick, currentSlide, nextSlide) {
+          var $animatingElements = $('div.slick-slide[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+          doAnimations($animatingElements);    
+});
+  function doAnimations(elements) {
+    var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    elements.each(function() {
+        var $this = $(this);
+        var $animationDelay = $this.data('delay');
+        var $animationType = 'animated ' + $this.data('animation');
+        $this.css({
+            'animation-delay': $animationDelay,
+            '-webkit-animation-delay': $animationDelay
+        });
+        $this.addClass($animationType).one(animationEndEvents, function() {
+            $this.removeClass($animationType);
+        });
+    });
+}
+});
